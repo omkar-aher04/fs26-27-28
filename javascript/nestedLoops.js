e13bb007814b2620126a08182103c59d2b0c58bf
rohitjainfsl <rohit@fullstacklearning.com>
2025-02-07 22:02:39 +0530

    js day 3

---
+++ javascript/nestedLoops.js
@@ -0,0 +1,31 @@
+// let x = 1;
+// for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 3; j++) {
+//     x = x + 1;
+//   }
+// }
+// console.log(x); //10
+
+// let x = 1;
+// for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 3; j++) {
+//     x = x + i;
+//   }
+// }
+// console.log(x); //19
+
+// let x = 1;
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = i + 1; j <= 5; j++) {
+//     x++;
+//   }
+// }
+// console.log(x); //11
+
+// let x = 1;
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = i - 1; j <= 5; j++) {
+//     x = x + i;
+//   }
+// }
+// console.log(x); //21
