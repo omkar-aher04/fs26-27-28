132223fc9fadbdad1c837ac699605efebb91aeb8
rohitjainfsl <rohit@fullstacklearning.com>
2025-02-25 22:02:37 +0530

    day 11 dom 4

--- javascript/notes.js
+++ javascript/notes.js
@@ -1,51 +1,87 @@
 const form = document.querySelector("form");
-const textarea = document.querySelector("textarea");
-const color = document.querySelector("input");
+const textarea = form.querySelector("textarea");
+const color = form.querySelector("input");
 const notesContainer = document.querySelector(".notesContainer");
 const undo = document.querySelector(".undo");
 const createdNotes = [];
 const deletedNotes = [];
 
+window.addEventListener("load", undoButtonStatus);
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  const newNote = {
+    text: textarea.value,
+    color: color.value,
+    timestamp: new Date().toLocaleString(),
+    position: Date.now(),
+  };
 
-  const noteObject = {};
+  createdNotes.push(newNote);
+  // console.log(createdNotes);
+  displayNotes();
 
-  noteObject.timestamp = new Date().toLocaleString();
-
-  const note = document.createElement("div"); //<div>
-  const close = document.createElement("span");
-  const dateTime = document.createElement("span");
-
-  note.classList.add("note"); //<div class="note">
-  close.classList.add("close");
-  dateTime.classList.add("timestamp");
-
-  note.innerText = textarea.value; //<div class="note">Hello World
-  close.innerHTML = "&times;"; //x
-  dateTime.innerText = noteObject.timestamp;
-
-  close.addEventListener("click", (e) => {
-    // e.target.parentElement.remove();
-    deletedNotes.push(e.target.closest(".note"));
-    e.target.closest(".note").remove();
-  });
-
-  note.style.backgroundColor = color.value; //<div class="note" style="background-color: "red">Hello World
-
-  note.append(close, dateTime);
-
-  noteObject.note = note;
-  noteObject.position = notesContainer.children.length;
-
-  createdNotes.push(noteObject);
-
-  notesContainer.append(note); // UI MANIPULATION
-
-  console.log(createdNotes);
+  textarea.value = "";
+  textarea.focus();
 });
+
+function displayNotes() {
+  notesContainer.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+  createdNotes.forEach((note) => {
+    const noteDiv = document.createElement("div");
+    noteDiv.classList.add("note");
+    noteDiv.style.backgroundColor = `${note.color}`;
+
+    const text = document.createElement("p");
+    // text.innerText = note.text;
+    text.innerText = `${note.text}`; //TEMPLATE LITERAL
+
+    const close = document.createElement("span");
+    close.classList.add("close");
+    close.innerHTML = "&times;";
+
+    close.addEventListener("click", (e) => {
+      const indexToDelete = createdNotes.findIndex((n) => {
+        return n.position === note.position;
+      });
+      deletedNotes.push(...createdNotes.splice(indexToDelete, 1));
+
+      e.target.closest(".note").remove();
+      console.log("created: ", createdNotes);
+      console.log("deleted: ", deletedNotes);
+
+      undoButtonStatus();
+    });
+
+    const timestamp = document.createElement("span");
+    timestamp.classList.add("timestamp");
+    timestamp.innerText = `${note.timestamp}`;
+
+    noteDiv.append(text, close, timestamp);
+    fragment.append(noteDiv);
+  });
+  notesContainer.append(fragment);
+}
 
 undo.addEventListener("click", (e) => {
   const lastDeletedNote = deletedNotes.pop();
-  notesContainer.append(lastDeletedNote);
+  createdNotes.push(lastDeletedNote);
+
+  createdNotes.sort((a, b) => {
+    return a.position - b.position;
+  });
+
+  console.log("created: ", createdNotes);
+  console.log("deleted: ", deletedNotes);
+
+  displayNotes();
+  undoButtonStatus();
 });
+
+function undoButtonStatus() {
+  undo.disabled = deletedNotes.length === 0 ? true : false;
+}
+
+
+
