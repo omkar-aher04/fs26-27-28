7fdd9cce6f7af30516e265ba3b83d623f9cb573a
rohitjainfsl <rohit@fullstacklearning.com>
2025-02-27 21:59:35 +0530

    day 13 async programming init

---
+++ javascript/async programming/setTimeout.js
@@ -0,0 +1,26 @@
+// const btn = document.querySelector("button");
+// btn.addEventListener("click", printName);
+
+// function printName() {
+//   //   console.log("Javascript");
+
+//   //   setTimeout(() => {
+//   //     console.log("Node");
+//   //   }, 3000);
+
+//   //   console.log("React");
+
+//   setInterval(() => console.log("Dobara chalaya"), 2000);
+// }
+
+// console.log("Hello");
+// console.log("Vanakkam");
+// console.log("Namaste");
+
+// console.log("Hello");
+// setTimeout(() => console.log("Vanakkam"), 500);
+// console.log("Bonjour");
+// setTimeout(() => console.log("Namaste ri"), 499);
+
+
+
