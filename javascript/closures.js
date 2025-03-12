e4a3887ca915af79e063caa15a65aa5da11c1e46
rohitjainfsl <rohit@fullstacklearning.com>
2025-03-12 21:58:30 +0530

    closures 2

--- javascript/closures.js
+++ javascript/closures.js
@@ -19,7 +19,7 @@
 
 // console.log(outer()()()); //CURRYING
 
-// USE CASE: Data Encapsulation
+// // USE CASE: Data Encapsulation
 
 // function counter() {
 //   let count = 0; // Private variable, defined in the lexical scope
@@ -39,92 +39,186 @@
 //   };
 // }
 
-// const myCounter = counter();
-// myCounter.increment(); // 1
-// myCounter.increment(); // 2
-// myCounter.decrement(); // 1
-// myCounter.getValue();
+// // const myCounter = counter();
+// // myCounter.increment(); // 1
+// // myCounter.increment(); // 2
+// // myCounter.decrement(); // 1
+// // myCounter.getValue();
 
-// // count variable is not directly accessible
-// console.log(myCounter.count); // undefined
+// // // count variable is not directly accessible
+// // console.log(myCounter.count); // undefined
 
-// function createMultiplier(multiplier) {
-//   return function (value) {
-//     return value * multiplier;
+// // function createMultiplier(multiplier) {
+// //   return function (value) {
+// //     return value * multiplier;
+// //   };
+// // }
+
+// // const double = createMultiplier(2);
+// // const triple = createMultiplier(3);
+
+// // console.log(double(3)); //6
+// // console.log(triple(6)); //18
+
+// // function createCounter() {
+// //   let count = 0;
+// //   return function () {
+// //     count++;
+// //     console.log(count);
+// //   };
+// // }
+
+// // const counter = createCounter();
+// // counter();
+// // counter();
+// // counter();
+
+// // function createUser(name) {
+// //     let score = 0;
+
+// //     return {
+// //       getName: function () {
+// //         return name;
+// //       },
+// //       increaseScore: function () {
+// //         score++;
+// //       },
+// //       getScore: function () {
+// //         return score;
+// //       }
+// //     };
+// //   }
+
+// //   const user = createUser("Alice");
+// //   console.log(user.getName());
+// //   user.increaseScore();
+// //   user.increaseScore();
+// //   console.log(user.getScore());
+
+// // function delayedLogger() {
+// //   for (var i = 1; i <= 3; i++) {
+// //     setTimeout(function () {
+// //       console.log(i);
+// //     }, 1000);
+// //   }
+// // }
+
+// // delayedLogger();
+
+// function once(func) {
+//   let executed = false;
+//   return function (...args) {
+//     console.log("args", args);
+//     if (!executed) {
+//       executed = true;
+//       return func(...args);
+//     }
 //   };
 // }
 
-// const double = createMultiplier(2);
-// const triple = createMultiplier(3);
+// const sayHello = once((fname, lname) =>
+//   console.log(`Hello, ${fname} ${lname}!`)
+// );
 
-// console.log(double(3)); //6
-// console.log(triple(6)); //18
+// sayHello("John", "Cena");
+// sayHello("Doe");
 
-// function createCounter() {
+// // DEBOUNCING & THROTTLING
+
+// // PROTOTYPE, PROTOTYPAL INHERITANCE, POLYFILLS
+
+// function createListener(eventName) {
+//   return function (callback) {
+//     console.log(`Listening for ${eventName}`);
+//     callback();
+//   };
+// }
+
+// const clickListener = createListener("click");
+// clickListener(() => console.log("Click event triggered!"));
+
+// function createCounter(step) {
 //   let count = 0;
 //   return function () {
-//     count++;
+//     count += step;
 //     console.log(count);
 //   };
 // }
 
-// const counter = createCounter();
+// const counter = createCounter(5);
 // counter();
 // counter();
 // counter();
 
-// function createUser(name) {
-//     let score = 0;
-
-//     return {
-//       getName: function () {
-//         return name;
-//       },
-//       increaseScore: function () {
-//         score++;
-//       },
-//       getScore: function () {
-//         return score;
-//       }
-//     };
-//   }
-
-//   const user = createUser("Alice");
-//   console.log(user.getName());
-//   user.increaseScore();
-//   user.increaseScore();
-//   console.log(user.getScore());
-
-// function delayedLogger() {
-//   for (var i = 1; i <= 3; i++) {
-//     setTimeout(function () {
-//       console.log(i);
-//     }, 1000);
-//   }
+// function uniqueIDGenerator() {
+//   let id = 0;
+//   return function () {
+//     id++;
+//     return `id-${id}`;
+//   };
 // }
 
-// delayedLogger();
+// const getID = uniqueIDGenerator();
+// console.log(getID());
+// console.log(getID());
+// console.log(getID());
 
-function once(func) {
-  let executed = false;
-  return function (...args) {
-    console.log("args", args);
-    if (!executed) {
-      executed = true;
-      return func(...args);
-    }
-  };
-}
+// function delayedMessage(msg, delay) {
+//   return function () {
+//     setTimeout(() => console.log(msg), delay);
+//   };
+// }
 
-const sayHello = once((fname, lname) =>
-  console.log(`Hello, ${fname} ${lname}!`)
-);
+// const sayHello = delayedMessage("Hello, World!", 2000);
+// console.log("Waiting...");
+// sayHello();
 
-sayHello("John", "Cena");
-sayHello("Doe");
+// var print;
 
+// for (let i = 0; i < 3; i++) {
+//   print = function () {
+//     console.log(i); //0 1 2
+//   };
+// }
 
-// DEBOUNCING & THROTTLING
+// var myObject = { print: print };
+// myObject.print(); //3 2
 
+// for (var i = 0; i < 3; i++) {
+//   console.log("Channu");
+//   setTimeout(function() {
+//       console.log(i);
+//   }, 1000);
+// }
 
-// PROTOTYPE, PROTOTYPAL INHERITANCE, POLYFILLS
+//IIFE
+
+// for (var i = 0; i <= 3; i++) {
+//   (function (j) {
+//     setTimeout(function () {
+//       console.log(j);
+//     }, 1000);
+//   })(i);
+// }
+
+// for (let i = 0; i <= 3; i++) {
+//   function channu(j) {
+//     console.log(j);
+//   }
+//   channu(i);
+// }
+
+// console.log("Start");
+
+// setTimeout(function() {
+//     console.log("Timeout");
+// }, 0);
+
+// console.log("End");
+
+// let count = 0;
+// let interval = setInterval(function () {
+//   console.log("Hello");
+//   count++;
+//   if (count === 3) clearInterval(interval);
+// }, 500);
