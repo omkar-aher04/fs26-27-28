fb7d4e4367f97d85a1809d77bd283bc437a1442d
vikas <>
2025-04-14 18:08:08 +0530

    adding toggle functionality

--- react/learnreact/src/boxesShowHide/Box.jsx
+++ react/learnreact/src/boxesShowHide/Box.jsx
@@ -1,5 +1,6 @@
 import { FaChevronUp } from "react-icons/fa";
  import { useState } from "react";
+ 
 function Box({ heading }) {
    
   const [show, setShow] = useState(true);
@@ -11,7 +12,7 @@ function Box({ heading }) {
     <div className="box h-48 w-1/4 border-2 border-amber-300 rounded-lg">
       <h2 className="p-3 h-1/4 text-2xl bg-gray-400 font-bold flex justify-between items-center">
         {heading}
-        <FaChevronUp className="cursor-pointer"  onClick={toggleContent}/>
+        <FaChevronUp className={`box cursor-pointer ${show ? 'rotate' : ''}`}  onClick={toggleContent}/>
       </h2>
 
       {show && (<p className="p-3 h-3/4">
